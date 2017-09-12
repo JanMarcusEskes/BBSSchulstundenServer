6193269307258f68d4f337f6fe8011264463ffb3
Jan-Marcus Eskes <jan-marcus@eskes.de>
2017-09-12 21:51:23 +0200

    Fixed - Empty User or Password Crash

--- Server/Source/Settings.cs
+++ Server/Source/Settings.cs
@@ -36,7 +36,7 @@ namespace Server.Source
                 return temp;
             }
             else
-                return null;
+                return "";
         }
         public void Passwort(string newPasswort)
         {
@@ -59,7 +59,7 @@ namespace Server.Source
                 return temp;
             }
             else
-                return null;
+                return "";
         }
         public void Save(Settings Einstellungen)
         {
